18c02c74af9fda6954d3b006f82c7ab6ad990ad9
Niki Kotecha <nikikotecha6@gmail.com>
2024-06-13 08:52:52 +0100

    changes

--- entropyplotts.py
+++ entropyplotts.py
@@ -32,12 +32,12 @@ def training_figures(file_paths_list, iteration_to_check, number_agents):
         all_entropies.append(entropy_vales)
 
 
-    fig, ax = plt.subplots(1,1,figsize=(10,6), layout = 'constrained')
+    fig, ax = plt.subplots(1,1,figsize=(12,8), layout = 'constrained')
     cols = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown']
     #cols = ['tab:blue', 'tab:red', 'tab:brown']
     for sublist, col in zip(all_entropies, cols): 
         ax.plot(sublist, color = col, linestyle = 'solid')
-    ax.legend(['0.0', '0.1', '0.2', '0.5', '1.0', '2.0'], frameon=False, fontsize=14)
+    ax.legend(['0.0', '0.5', '2.0', '0.5', '1.0', '2.0'], frameon=False, fontsize=14)
     #ax.spines['right'].set_visible(False)
     #ax.spines['top'].set_visible(False)
     ax.set_xlabel('Training Iteration')
@@ -49,41 +49,24 @@ def training_figures(file_paths_list, iteration_to_check, number_agents):
     return all_entropies
 
 
-mappo_24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-41-461marte8k\result.json"]
-ippo_24 =  [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-42-44nu1jjpuk\result.json"]
-gmappo_24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-45-588zbz5wvr\result.json"]
-g2_noise0 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-07_08-10-33cjpows2l\result.json"]
-g2_noise1 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-07_08-35-34vpwck8mj\result.json"]
-
-file_paths_24 = [mappo_24, ippo_24, gmappo_24, g2_noise0, g2_noise1]
-
-
-mappo_12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-02_13-36-43l0j31ce0\result.json"]
-ippo_12 =  [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-12-06owdt78yd\result.json"]
-gmappo_12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-13-281w63y_j_\result.json"]
-g2_12_noise0 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_12-00-15zbm_m4ax\result.json"]
-g2_12_noise1 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-11-11wakdzoi7\result.json"]
-
-file_paths_12 = [mappo_12, ippo_12, gmappo_12, g2_12_noise0, g2_12_noise1]
-
-ippo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-11_09-18-09gpdriji_\result.json"]
-mappo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_20-41-03g7yt7xc3\result.json"]
-gmappo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_20-40-58vb6xruob\result.json"]
-g2_18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_22-16-53zax54im2\result.json"]
-g2_18noise = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k\result.json"]
+ippo18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-11_09-18-09gpdriji_/result.json"]
+mappo18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-10_20-41-03g7yt7xc3/result.json"]
+gmappo18 = [r"ay_results/PPO_MultiAgentInvManagementDiv_2024-04-10_20-40-58vb6xruob/result.json"]
+g2_18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-10_22-16-53zax54im2/result.json"]
+g2_18noise = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k/result.json"]
 file_paths18 = [ippo18, mappo18, gmappo18, g2_18, g2_18noise]
 
-noise_18_0 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-12_18-09-12pea61thx\result.json"]
-noise_18_01 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-12_18-10-223euvrde2\result.json"]
-noise_18_02 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k\result.json"]
-noise_18_05 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-14_13-47-33xyca2ajl\result.json"]
-noise_18_75 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-15_11-16-012_kinfxk\result.json"]
-noise_18_10 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-14_13-48-08fskh2eg9\result.json"]
-noise_18_20 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-16_10-29-44k80w0cni\result.json"]
+noise_18_0 = [r"C:ray_results/PPO_MultiAgentInvManagementDiv_2024-05-12_18-09-12pea61thx/result.json"]
+noise_18_01 = [r"C:ray_results/PPO_MultiAgentInvManagementDiv_2024-05-12_18-10-223euvrde2/result.json"]
+noise_18_02 = [r"C:ray_results/PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k/result.json"]
+noise_18_05 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-14_13-47-33xyca2ajl/result.json"]
+noise_18_75 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-15_11-16-012_kinfxk/result.json"]
+noise_18_10 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-14_13-48-08fskh2eg9/result.json"]
+noise_18_20 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-16_10-29-44k80w0cni/result.json"]
 file_paths_noise_all  = [noise_18_0, noise_18_01, noise_18_02, noise_18_05, noise_18_10, noise_18_20]
 file_paths_noise = [noise_18_0, noise_18_05, noise_18_20]
 
 iteration_to_check = 60 
 number_agents = [18,18,18,18,18]
 
-training_figures(file_paths_noise_all, iteration_to_check, number_agents)
+training_figures(file_paths_noise, iteration_to_check, number_agents)
--- trainingfig.py
+++ trainingfig.py
@@ -4,48 +4,41 @@ from statistics import mean
 import matplotlib.pyplot as plt
 import numpy as np
 import os 
-<<<<<<< HEAD
-import statistics
-
-'''
-todo: add checkpoint to the runn.py file so you can access the compute_action mean & sd
-will need to restore the checkpoint on here 
-'''
-=======
 from matplotlib.colors import LinearSegmentedColormap
 from scipy import stats
 from torch import layout
 
-ippo6 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-19_19-47-56f6z96d1b\result.json"]
-mappo6 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-19_19-47-15u94ac_tc\result.json"]
-gmappo6 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-19_19-49-37sbsnq6km\result.json"]
-g2_6 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-19_23-09-53h3ojljkh\result.json"]
-g2_6noise = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-19_23-11-22vjup3rv8\result.json"]
+plt.rcParams['text.usetex'] = True
+
+ippo6 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-19_19-47-56f6z96d1b/result.json"]
+mappo6 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-19_19-47-15u94ac_tc/result.json"]
+gmappo6 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-19_19-49-37sbsnq6km/result.json"]
+g2_6 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-19_23-09-53h3ojljkh/result.json"]
+g2_6noise = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-19_23-11-22vjup3rv8/result.json"]
 file_paths6 = [ippo6, mappo6, gmappo6, g2_6, g2_6noise]
 
-ippo12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-12-06owdt78yd\result.json"]
-mappo12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-02_13-36-43l0j31ce0\result.json"]
-gmappo12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-13-281w63y_j_\result.json"]
-g2_12 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_12-00-15zbm_m4ax\result.json"]
-g2_12noise = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-01_16-11-11wakdzoi7\result.json"]
+ippo12 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-01_16-12-06owdt78yd/result.json"]
+mappo12 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-02_13-36-43l0j31ce0/result.json"]
+gmappo12 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-01_16-13-281w63y_j_/result.json"]
+g2_12 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-01_12-00-15zbm_m4ax/result.json"]
+g2_12noise = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-01_16-11-11wakdzoi7/result.json"]
 file_paths12 = [ippo12, mappo12, gmappo12, g2_12, g2_12noise]
 
-ippo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-11_09-18-09gpdriji_\result.json"]
-mappo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_20-41-03g7yt7xc3\result.json"]
-gmappo18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_20-40-58vb6xruob\result.json"]
-g2_18 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-10_22-16-53zax54im2\result.json"]
-g2_18noise = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k\result.json"]
+ippo18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-11_09-18-09gpdriji_/result.json"]
+mappo18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-10_20-41-03g7yt7xc3/result.json"]
+gmappo18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-10_20-40-58vb6xruob/result.json"]
+g2_18 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-10_22-16-53zax54im2/result.json"]
+g2_18noise = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-15_11-14-05l1k6u06k/result.json"]
 file_paths18 = [ippo18, mappo18, gmappo18, g2_18, g2_18noise]
 
-ippo24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-42-44nu1jjpuk\result.json"]
-mappo24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-41-461marte8k\result.json"]
-gmappo24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-06_15-45-588zbz5wvr\result.json"]
-g2_24 = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-04-26_09-21-552nxyn_hr\result.json"]
-g2_24noise = [r"C:\Users\nk3118\ray_results\PPO_MultiAgentInvManagementDiv_2024-05-07_08-35-34vpwck8mj\result.json"]
+ippo24 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-06_15-42-44nu1jjpuk/result.json"]
+mappo24 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-06_15-41-461marte8k/result.json"]
+gmappo24 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-06_15-45-588zbz5wvr/result.json"]
+g2_24 = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-04-26_09-21-552nxyn_hr/result.json"]
+g2_24noise = [r"ray_results/PPO_MultiAgentInvManagementDiv_2024-05-07_08-35-34vpwck8mj/result.json"]
 
 file_paths24 = [ippo24, mappo24, gmappo24, g2_24, g2_24noise]
 
->>>>>>> 6784ca85ba80f2b20d30d6d8186ace78606f4133
 def normalize_rewards(rewards, num_agents):
     return [reward / num_agents for reward in rewards]
 
@@ -58,12 +51,6 @@ def normalize_rewards2(rewards):
 
 def training_figures(file_paths_list, iteration_to_check, number_agents):
 
-<<<<<<< HEAD
-    mean_time_iters = []
-    std_time_iters = []
-    for file_paths , label, no_agent in zip(file_paths_list, ['ng1_3n2p', 'ng2_3n2p', 'g1_3n2p'], number_agents):
-        all_rewards = []
-=======
     mean_training_times = []
     stds_training_times = []
     all_avg_rewards = []
@@ -81,25 +68,13 @@ def training_figures(file_paths_list, iteration_to_check, number_agents):
             for json_obj in json_list:
                 if json_obj.strip():
                     results_list.append(json.loads(json_obj))
-                    
-            time_step = []
+
             episode_reward_mean = []
             time_step = []
             for result in results_list:
                 iteration = result['training_iteration']
                 episode_reward_mean.append(result['episode_reward_mean'])
                 time_step.append(result['time_this_iter_s'])
-<<<<<<< HEAD
-
-            mean_time = np.mean(time_step)
-            mean_time_iters.append(mean_time)
-            std_time = np.std(time_step)
-            std_time_iters.append(std_time)
-
-            #normalized_rewards = normalize_rewards(episode_reward_mean, no_agent)
-            normalized_rewards = normalize_rewards2(episode_reward_mean)
-            all_rewards.append(normalized_rewards)
-=======
 
             time_step = np.array(time_step)
             z_scores = stats.zscore(time_step)
@@ -114,7 +89,6 @@ def training_figures(file_paths_list, iteration_to_check, number_agents):
 
         mean_training_times.append(np.mean(mean_training_times_path))
         stds_training_times.append(np.mean(stds_training_times_path))
->>>>>>> 6784ca85ba80f2b20d30d6d8186ace78606f4133
 
         max_iterations = max(len(rewards) for rewards in all_rewards)
         padded_rewards = [r + [np.nan] * (max_iterations - len(r)) for r in all_rewards]
@@ -124,29 +98,18 @@ def training_figures(file_paths_list, iteration_to_check, number_agents):
         iteration_index = min(iteration_to_check, max_iterations - 1)
         highest_avg_reward_path = file_paths[np.argmax(avg_reward[iteration_index])]
         all_avg_rewards.append(avg_reward)
-    print(mean_training_times)
     return highest_avg_reward_path, mean_training_times, stds_training_times, all_avg_rewards, std_reward
 
 
 def plots(file_paths_list):
     colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown']
     labels = ['IPPO', 'MAPPO', 'G-MAPPO', 'GP-MAPPO', 'Noise GP-MAPPO', 'N/A']
-    n_sets = len(file_paths_list)
-    fig, axes = plt.subplots(2, 2, figsize=(18, 12), constrained_layout=True)
-    axes = axes.flatten()
-
-    if n_sets == 1:
-        axes = [axes]  # Ensure axes is always iterable
-    labels_pos = ['a)', 'b)', 'c)', 'd)']
-
-    for ax, label in zip(axes.flatten(), labels_pos):
-        ax.text(0.5, -0.2, label, transform=ax.transAxes, 
-            fontsize=14, va='center', ha='center')
+    
         
     for i, file_paths in enumerate(file_paths_list):
         highest_avg_reward_path, mean_training_times, stds_training_times, all_avg_rewards, std_reward = training_figures(file_paths, 100, [1,1,1,1,1])
         iterations = range(len(all_avg_rewards[0]))
-        ax = axes[i]
+        fig, ax = plt.subplots(figsize=(12, 8), layout = 'constrained')
         for j, (avg_reward, color, label) in enumerate(zip(all_avg_rewards, colors, labels)):
             ax.plot(iterations, avg_reward, label=label, color=color)
             # Uncomment to add shaded area for std deviation
@@ -157,8 +120,10 @@ def plots(file_paths_list):
         ax.legend(frameon=False, fontsize=12)
         #ax.spines['right'].set_visible(False)
         #ax.spines['top'].set_visible(False)
-    fig.savefig('training_compile1.png', dpi=1100)
-    plt.show()
+        names = ['train6.png', 'train12.png', 'train18.png', 'train24.png']
+        for name in names: 
+            fig.savefig(name, dpi=1100)
+            plt.show()
 
 
 file_paths_list = [file_paths6, file_paths12, file_paths18, file_paths24]
@@ -182,7 +147,7 @@ def error_bars_method(file_paths_list, number_agents_list):
     #ax.spines['right'].set_visible(False)
     #ax.spines['top'].set_visible(False)
     plt.tight_layout()
-    fig.savefig('error_bars_compile.png', dpi=1100)
+    #fig.savefig('error_bars_compile.png', dpi=1100)
     plt.show()
 
 number_agents_list = [6, 12, 18, 24]
@@ -213,5 +178,4 @@ def error_bars_method1(file_paths_list):
     axes[0].set_ylabel('Mean Time per Iteraton (s)')
     axes[-1].set_xlabel('Agents')
     plt.tight_layout()
-    plt.show()
     plt.show()
